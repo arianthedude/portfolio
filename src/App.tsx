a96e16d046a04057f9e921a16529c610afb08b0c
arian the dude <arian.sickmind@yahoo.com>
2026-06-22 13:34:41 +0330

    trying to fix the issue with img path.

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ export default function App() {
       <Header
         name={t("name")}
         title={t("title")}
-        avatarUrl="./assets/img.jpeg"
+        avatarUrl="/img.jpeg"
         onScrollToSection={handleScrollToSection}
         onPrintResume={handlePrintResume}
       />
